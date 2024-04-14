60b1340a0acc67315f5c412e73016a283fec2f00
umaisayan <armughanrehman2018@gmail.com>
2024-04-13 22:23:13 -0700

    file update

---
+++ loops-1.js
@@ -0,0 +1,21 @@
+// 1-ans
+for (let i = 0; i <= 100; i++) {
+      console.log("hello!");
+}
+// 2-ans
+for (let count = 1; count <= 1000; count++) {
+      console.log(count);
+}
+// 3-ans
+for (let count = 1000; count >= 1; count--) {
+      console.log(count);
+}
+// 4-ans
+for (let sum = 1; sum <= 100; sum++) {
+      console.log(sum);
+}
+//5-ans
+for (let i = 1; i <= 10; i++) {
+      console.log(numb * i);
+}
+
